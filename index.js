544cffbdedb01017c8ce1c64164602aa9d18ac31
sonjuhyeon <ths120521113105@daum.net>
2024-09-13 12:45:45 +0900

    update url

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const PORT = "8080";
 
 const app = express(); // express 모듈을 사용하기 위해 app 변수에 할당
 
-// const corsOptions = {
-//   origin: "http://localhost:3000", // 허용할 주소
-//   credentials: true, // 인증 정보 허용
-// };
+app.use(
+  cors({
+    origin: "https://taskfront.microdeveloper.co.kr",
+    credentials: true,
+  })
+);
 
-// const corsOption2 = ["http://localhost:3000", "http://localhost:3001"];
-
-app.use(cors()); // http, https 프로토콜을 사용하는 서버간의 통신을 허용
 app.use(express.json()); // express 모듈의 json() 메소드를 사용
 
 app.get("/", (request, response) => {
